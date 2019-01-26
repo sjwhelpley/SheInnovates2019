5448c4d07ab6ebe9c3efb69542133e939b7cf4ad
margstuart <43256357+margstuart@users.noreply.github.com>
2019-01-26 16:02:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from app import app
 from db_setup import init_db, db_session
-from forms import CompanySearchForm
+from forms import CompanySearchForm, StatsForm
 from flask import flash, render_template, request, redirect
 from models import Stats
 #from flask import Flask, render_template, request, redirect
@@ -31,13 +31,34 @@ def search_results(search):
   if not results:
     flash("No results found")
     return redirect("/")
-  else:
-    return render_template("results.html", results=results)
+  else: #show the results
+    return render_template("results.html", table=table)
+    #return render_template("results.html", results=results)
+
+@app.route("/new_company", methods=["GET", "POST"])
+def new_company():
+  form = StatsForm(request.form)
+
+  if request.method == "POST" and form.validate():
+    stats = Stats()
+    save_changes(stats, form, new=True)
+    flash("Company created!")
+    return redirect("/")
+  return render_template("new_company.html", form=form)
+
+def save_changes(stats, form, new=False):
+  company = Company()
+  company.name = form.company.data
+
+  stats.company = company
+  stats.board = form.board.data
+  stats.ticker = form.ticker.data #Name
+  if new:
+    db_session.add(stats)
+
+  db_session.commit()
+
 
 if __name__ == "__main__": #for debugging
     app.run(debug=True)
-
-
-
-
 
